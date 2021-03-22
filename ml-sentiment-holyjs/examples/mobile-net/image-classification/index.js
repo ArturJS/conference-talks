2fc4ac28d86584692bdb770d0b5bbd41652d022b
ArturJS <nizamutdin.art@gmail.com>
2021-03-22 23:39:06 +0400

    Image classification [Part 2]

--- ml-sentiment-holyjs/examples/mobile-net/image-classification/index.js
+++ ml-sentiment-holyjs/examples/mobile-net/image-classification/index.js
@@ -1,48 +1,14 @@
+const tfnode = require('@tensorflow/tfjs-node');
+const mobilenet = require('@tensorflow-models/mobilenet');
 const fs = require('fs').promises;
 const path = require('path');
-const mobilenet = require('@tensorflow-models/mobilenet');
-const tfnode = require('@tensorflow/tfjs-node');
-const jimp = require('jimp');
-const sizeOf = require('image-size');
 
-const readImage = async (filePath) => {
-  console.log('Loading and decoding image...');
-  await jimp.read(filePath).then((lenna) => {
-    const dimensions = sizeOf(filePath);
-    const side = 3136;
-    return new Promise((resolve, reject) => {
-      lenna
-        .resize(side, side / (dimensions.width / dimensions.height))
-        .write(filePath + 'resized.png', (err) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve();
-        });
-    });
-  });
-  const imageBuffer = await fs.readFile(filePath + 'resized.png');
-
-  console.log('imageBuffer:');
-  console.log(imageBuffer.length);
-  //Given the encoded bytes of an image,
-  //it returns a 3D or 4D tensor of the decoded image. Supports BMP, GIF, JPEG and PNG formats.
-  const tfImage = tfnode.node.decodeImage(imageBuffer);
-  return tfImage;
-};
-
-const imageClassification = async (imagePath) => {
-  const image = await readImage(imagePath);
-
-  console.log('Loading mobilenet...');
-  const mobilenetModel = await mobilenet.load();
-
-  console.log('Image classification...');
-  const predictions = await mobilenetModel.classify(image);
-
-  console.log('Classification Results:', predictions);
+const classify = async (imagePath) => {
+  const image = await fs.readFile(imagePath);
+  const decodedImage = tfnode.node.decodeImage(image, 3);
+  const model = await mobilenet.load();
+  const predictions = await model.classify(decodedImage);
+  console.log('predictions:', predictions);
 };
 
 // @ts-ignore
@@ -50,16 +16,16 @@ const isMainModule = typeof require !== 'undefined' && require.main === module;
 
 if (isMainModule) {
   if (process.argv.length !== 3)
-    throw new Error('Incorrect arguments: node classify.js <IMAGE_FILE>');
+    throw new Error('Incorrect arguments: node classify.js <IMAGE_FILE_PATH>');
 
-  imageClassification(process.argv[2]);
+  classify(process.argv[2]);
 }
 
 module.exports = {
   run() {
-    console.log('Image classification running...');
-    imageClassification(
-      process.argv[2] || path.resolve(__dirname, './snail.png'),
-    ).catch(console.error);
+    const imagePath = path.join(__dirname, './snail.png');
+    classify(imagePath).catch(console.error);
   },
 };
+
+// see also https://gist.github.com/jthomas/145610bdeda2638d94fab9a397eb1f1d#gistcomment-3416190
--- ml-sentiment-holyjs/main.js
+++ ml-sentiment-holyjs/main.js
@@ -2,25 +2,29 @@ const { Select } = require('enquirer');
 const simpleNeuralNetwork = require('./examples/simple-neural-network');
 const imageClassification = require('./examples/mobile-net/image-classification');
 
-const exampleNeuralNetworks = {
-  'simple-neural-network': simpleNeuralNetwork,
-  'mobile-net\\image-classification': imageClassification,
-  'sentiment-ai': {
-    run() {
-      throw new Error('Error: ToDo Implement :-/');
+function start() {
+  const exampleNeuralNetworks = {
+    'simple-neural-network': simpleNeuralNetwork,
+    'mobile-net\\image-classification': imageClassification,
+    'sentiment-ai': {
+      run() {
+        throw new Error('Error: ToDo Implement :-/');
+      },
     },
-  },
-};
+  };
 
-const prompt = new Select({
-  name: 'color',
-  message: 'Pick an example:',
-  choices: Object.keys(exampleNeuralNetworks),
-});
+  const prompt = new Select({
+    name: 'color',
+    message: 'Pick an example:',
+    choices: Object.keys(exampleNeuralNetworks),
+  });
 
-prompt
-  .run()
-  .then((answer) => {
-    return exampleNeuralNetworks[answer].run();
-  })
-  .catch(console.error);
+  prompt
+    .run()
+    .then((answer) => {
+      return exampleNeuralNetworks[answer].run();
+    })
+    .catch(console.error);
+}
+
+start();
